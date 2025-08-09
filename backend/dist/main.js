43d1ed12f1366bbf306434b50f1f68aeebe539c1
sachinpatil <sachinaurad123@gmail.com>
2025-08-09 17:23:39 +0530

    fix: solved double hashing problem 1

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -8,7 +8,7 @@ async function bootstrap() {
     app.enableCors({
         origin: [
             'http://localhost:3000',
-            'https://allo-health-clinic.vercel.app/'
+            'https://allo-health-clinic.vercel.app'
         ],
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
         credentials: true,
